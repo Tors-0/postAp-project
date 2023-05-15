fc5b2346480f4b56c75b7ff085f2e63392290f8d
jankymuffinz <81887106+jankymuffinz@users.noreply.github.com>
2023-05-15 13:16:18 -0700

    adding simple gravity, still no constraints

--- src/VerletEngine/VerletSolver.java
+++ src/VerletEngine/VerletSolver.java
@@ -1,3 +1,34 @@
 public class VerletSolver {
+    Vector3d gravity = new Vector3d(0.0, 0.0, 10000.0);
   
+    
+    public void update(double dt) {
+      int subSteps = 4;
+      float subdt = dt/ (float) (subSteps);
+      
+      for(int i = subSteps; i--; ) {
+        applyGravity();
+        applyConstraint();
+        solveCollisions();
+        updatePositions(subdt);
+      }
+    }
+    
+    private void updatePositions(double dt) {
+     VerlPhysObjs.forEach(currentObj -> {
+       obj.updatePosition(dt);
+     })); 
+    }
+    
+    private void applyGravity() {
+      VerlPhysObjs.forEach(currentObj -> {
+          obj.accelerate(gravity);
+      });
+    }
+  
+    private void applyConstraints() {
+      
+      
+      
+    }
 }
