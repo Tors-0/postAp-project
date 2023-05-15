c5fb59a4b7aab376d55c624a0a074bd47c2ba1ce
jankymuffinz <81887106+jankymuffinz@users.noreply.github.com>
2023-05-15 12:06:35 -0700

    Create VerletObject.java

---
+++ src/VerletEngine/VerletObject.java
@@ -0,0 +1,10 @@
+public class VerletObject {
+  private Vector3d posCurrent;
+  private Vector3d posOld;
+  private Vector3d acceleration;
+  
+  
+  public updatePosition(float dt) {
+   Vector3d velocity 
+    
+  }
