bc9fc59c849d1aeab88bec79f80ff487e53da853
rae <raej0000@gmail.com>
2023-05-15 09:54:50 -0700

    add VectorUtils.java

--- jogl-v2.4.0/make/lib/swt/README.txt
+++
@@ -1,3 +0,0 @@
-Eclipse 4.26.0 Release Build: 4.26
-
-https://download.eclipse.org/eclipse/downloads/drops4/R-4.26-202211231800/
--- jogl-v2.4.0/make/scripts/adb-install-all-armv6.sh
+++
@@ -1,4 +0,0 @@
-#adb $* install ../../gluegen/build-android-armv6/jogamp-android-launcher.apk
-#adb $* install ../../gluegen/build-android-armv6/gluegen-rt-android-armv6.apk
-adb $* install ../build-android-armv6/jar/jogl-all-android-armv6.apk
-adb $* install ../build-android-armv6/jar/jogl-test-android.apk
